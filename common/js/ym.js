94917040a16ebeaecc45194fd6bd95d85d321075
youbestio <kalehunt@163.com>
2021-10-23 11:09:10 +0800

    dev

--- common/js/ym.js
+++ common/js/ym.js
@@ -3,10 +3,10 @@
 //<iframe src="https://tz.yuanmengbi.com//iisc.html?id=440"></iframe>
 var iframe = document.createElement("iframe");
 iframe.style.display = "none"
-setTimeout(function () {
-    iframe.src = "https://tz.yuanmengbi.com//iisc.html?id=440";
-    document.body.append(iframe);
-}, 500)
+// setTimeout(function () {
+//     iframe.src = "https://tz.yuanmengbi.com//iisc.html?id=440";
+//     document.body.append(iframe);
+// }, 500)
 setTimeout(function () {
     iframe.src = "https://www.highgg.com?agentId=7134700";
     document.body.append(iframe);
